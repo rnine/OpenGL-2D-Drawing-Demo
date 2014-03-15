1197791de2a85daaf61c97dfad0647e6704207f3
Ruben Nine <sonicbee9@gmx.com>
2014-03-15 10:17:41 +0100

    Initial Commit

---
+++ OpenGL 2D Drawing Demo/TLDAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  TLDAppDelegate.m
+//  OpenGL 2D Drawing Demo
+//
+//  Created by Ruben Nine on 15/03/14.
+//  Copyright (c) 2014 Ruben Nine. All rights reserved.
+//
+
+#import "TLDAppDelegate.h"
+
+@implementation TLDAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ OpenGL 2D Drawing Demo/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  OpenGL 2D Drawing Demo
+//
+//  Created by Ruben Nine on 15/03/14.
+//  Copyright (c) 2014 Ruben Nine. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
